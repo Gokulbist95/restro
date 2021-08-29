9743e53c87671eb54a9d2d1944092eb97ab9dccb
GokBeast <63182391+Gokulbist95@users.noreply.github.com>
2021-08-29 15:02:53 +0545

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,5 @@
+const burger = document.querySelector(".hamburger");
+const nav = document.querySelector(".nav-links")
+burger.addEventListener("click",() => {
+    nav.classList.toggle('show');
+});
