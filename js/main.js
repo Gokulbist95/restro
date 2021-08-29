1af0e91c92c7f4ed4cd3e06437992e2425bbf986
GokBeast <63182391+Gokulbist95@users.noreply.github.com>
2021-08-29 16:35:09 +0545

    Delete js directory

--- js/main.js
+++
@@ -1,5 +0,0 @@
-const burger = document.querySelector(".hamburger");
-const nav = document.querySelector(".nav-links")
-burger.addEventListener("click",() => {
-    nav.classList.toggle('show');
-});
